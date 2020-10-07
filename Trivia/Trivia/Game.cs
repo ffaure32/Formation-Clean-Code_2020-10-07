19407327d3078b12f96be4f87a9ff1de786001f9
ffaure <frederic.faure@ext.cdiscount.com>
2020-10-07 17:25:25 +0200

    deplacer logique purse

--- Trivia/Trivia/Game.cs
+++ Trivia/Trivia/Game.cs
@@ -53,25 +53,26 @@ namespace Trivia
 
         public void Roll(int roll)
         {
-            Console.WriteLine(_players[_currentPlayer] + " is the current player"); 
+            var currentPlayer = _players[_currentPlayer];
+            Console.WriteLine(currentPlayer + " is the current player"); 
             Console.WriteLine("They have rolled a " + roll);
 
 
-            if (_players[_currentPlayer].InPenaltyBox)
+            if (currentPlayer.InPenaltyBox)
             {
                 if (roll % 2 != 0)
                 {
                     //User is getting out of penalty box
                     _isGettingOutOfPenaltyBox = true;
                     //Write that user is getting out
-                    Console.WriteLine(_players[_currentPlayer] + " is getting out of the penalty box");
+                    Console.WriteLine(currentPlayer + " is getting out of the penalty box");
                     Move(roll);
                     LogPlayersInfo();
                     AskQuestion();
                 }
                 else
                 {
-                    Console.WriteLine(_players[_currentPlayer] + " is not getting out of the penalty box");
+                    Console.WriteLine(currentPlayer + " is not getting out of the penalty box");
                     _isGettingOutOfPenaltyBox = false;
                 }
             }
@@ -128,53 +129,51 @@ namespace Trivia
         /// <returns></returns>
         public bool WasCorrectlyAnswered()
         {
-            if (_players[_currentPlayer].InPenaltyBox)
+            var currentPlayer = _players[_currentPlayer];
+            if (currentPlayer.InPenaltyBox)
             {
                 if (_isGettingOutOfPenaltyBox)
                 {
                     Console.WriteLine("Answer was correct!!!!");
-                    _players[_currentPlayer].Purse++;
-                    Console.WriteLine(_players[_currentPlayer]
+                    currentPlayer.AddPoint();
+                    Console.WriteLine(currentPlayer
                             + " now has "
-                            + _players[_currentPlayer].Purse
+                            + currentPlayer.Purse
                             + " Gold Coins.");
 
-                    var winner = !(_players[_currentPlayer].Purse == pointsToWin);
-                    _currentPlayer++;
-                    if (_currentPlayer == _players.Count) 
-                    {
-                        _currentPlayer = 0;
-                    }
+                    var winner = !(currentPlayer.Purse == pointsToWin);
+                    NextPlayer();
 
                     return winner;
                 }
                 else
                 {
-                    _currentPlayer++;
-                    if (_currentPlayer == _players.Count) 
-                    {
-                        _currentPlayer = 0;
-                    }
+                    NextPlayer();
                     return true;
                 }
             }
             else
             {
                 Console.WriteLine("Answer was corrent!!!!");
-                _players[_currentPlayer].Purse++;
-                Console.WriteLine(_players[_currentPlayer]
+                currentPlayer.AddPoint();
+                Console.WriteLine(currentPlayer
                         + " now has "
-                        + _players[_currentPlayer].Purse
+                        + currentPlayer.Purse
                         + " Gold Coins.");
 
-                var winner = !(_players[_currentPlayer].Purse == pointsToWin);
-                _currentPlayer++;
-                if (_currentPlayer == _players.Count) 
-                {
-                    _currentPlayer = 0;
-                }
+                var winner = !(currentPlayer.Purse == pointsToWin);
+                NextPlayer();
 
                 return winner;
+            }
+        }
+
+        private void NextPlayer()
+        {
+            _currentPlayer++;
+            if (_currentPlayer == _players.Count)
+            {
+                _currentPlayer = 0;
             }
         }
 
@@ -188,11 +187,7 @@ namespace Trivia
             Console.WriteLine(_players[_currentPlayer] + " was sent to the penalty box");
             _players[_currentPlayer].InPenaltyBox = true;
 
-            _currentPlayer++;
-            if (_currentPlayer == _players.Count) 
-            {
-                _currentPlayer = 0;
-            }
+            NextPlayer();
             return true;
         }
 
--- Trivia/Trivia/Player.cs
+++ Trivia/Trivia/Player.cs
@@ -3,17 +3,21 @@
     public class Player
     {
         public bool InPenaltyBox { get; set; } = false;
-        public string Name { get; private set; }
+        public string Name { get; }
 
         public int Place { get; set; } = 0;
 
-        public int Purse { get; set; } = 0;
+        public int Purse { get; private set; } = 0;
 
         public Player(string name)
         {
             Name = name;
         }
 
+        public void AddPoint()
+        {
+            Purse++;
+        }
         public override string ToString()
         {
             return Name;
