2be1a8b7024928a9494534005d635331cd1b2976
Sylvain Camus <sylvain.camus@cdbdx.biz>
2020-10-07 16:22:21 +0200

    Class Player

---
+++ Trivia/Trivia/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace Trivia
+{
+    public class Player
+    {
+        public bool InPenaltyBox { get; private set; } = false;
+        public string Name { get; private set; }
+
+        public int Place { get; set; } = 0;
+
+        public int Purse { get; private set; } = 0;
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
